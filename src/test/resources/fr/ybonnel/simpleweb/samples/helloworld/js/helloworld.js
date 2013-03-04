82a1ab4e3167a672d45a0b8209cf7000de1642d0
ybonnel <ybonnel@gmail.com>
2013-03-04 15:17:43 +0100

    End of Hello World sample

--- src/test/resources/fr/ybonnel/simpleweb/samples/helloworld/js/helloworld.js
+++ src/test/resources/fr/ybonnel/simpleweb/samples/helloworld/js/helloworld.js
@@ -1,14 +1,28 @@
 
-function FormCtrl() {
-
+function FormCtrl($scope, $location) {
+    $scope.submitForm = function(name, times, color) {
+        if ($scope.form.$invalid) {
+            $scope.form.name.$dirty = true;
+            $scope.form.times.$dirty = true;
+        } else {
+            $location.path('/result/' + name + '/' + times + '/' + color);
+        }
+    }
 }
+FormCtrl.$inject = [ '$scope', '$location' ];
 
-function ResultCtrl() {
-
+function ResultCtrl($scope, $routeParams) {
+    $scope.name = $routeParams.name;
+    $scope.times = Array(parseInt($routeParams.times));
+    $scope.color = $routeParams.color;
+    if ( $routeParams.color === '') {
+        $scope.color = 'undefined';
+    }
 }
+ResultCtrl.$inject = [ '$scope', '$routeParams' ];
 
 angular.module('HelloWorld', []).config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/form', {templateUrl:'partial/form.html', controller:FormCtrl});
-    $routeProvider.when('/result', {templateUrl:'partial/result.html', controller:ResultCtrl});
+    $routeProvider.when('/result/:name/:times/:color', {templateUrl:'partial/result.html', controller:ResultCtrl});
     $routeProvider.otherwise({redirectTo: '/form'});
 }]);
