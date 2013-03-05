4e0a9b41951d0c0975d34eb6c01fd19054f509c7
ybonnel <ybonnel@gmail.com>
2013-03-05 09:23:07 +0100

    Add list page

--- src/test/resources/fr/ybonnel/simpleweb/samples/computers/js/computers.js
+++ src/test/resources/fr/ybonnel/simpleweb/samples/computers/js/computers.js
@@ -1,5 +1,6 @@
 
-function ListComputerCtrl($scope) {
+function ListComputerCtrl($scope, ComputerService) {
+    $scope.computers = ComputerService.query();
 }
 
 function NewComputerCtrl($scope) {
@@ -8,9 +9,15 @@ function NewComputerCtrl($scope) {
 function EditComputerCtrl($scope) {
 }
 
-angular.module('Computers', []).config(['$routeProvider', function($routeProvider) {
+angular.module('Computers', ['ComputersServices']).config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/computer', {templateUrl:'partial/list.html', controller:ListComputerCtrl});
     $routeProvider.when('/computer/new', {templateUrl:'partial/editOrNew.html', controller:NewComputerCtrl});
     $routeProvider.when('/computer/:id', {templateUrl:'partial/editOrNew.html', controller:EditComputerCtrl});
     $routeProvider.otherwise({redirectTo: '/computer'});
 }]);
+
+var services = angular.module('ComputersServices', ['ngResource']);
+
+services.factory('ComputerService', function($resource) {
+    return $resource('/computer/:id', {});
+});
