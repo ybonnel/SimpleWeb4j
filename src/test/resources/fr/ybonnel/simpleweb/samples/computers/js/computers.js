41c7552fa056bfcf0888ae6acaa9b3155156b598
ybonnel <ybonnel@gmail.com>
2013-03-05 18:06:49 +0100

    Create form for create computer

--- src/test/resources/fr/ybonnel/simpleweb/samples/computers/js/computers.js
+++ src/test/resources/fr/ybonnel/simpleweb/samples/computers/js/computers.js
@@ -9,12 +9,42 @@ function NewComputerCtrl($scope) {
 function EditComputerCtrl($scope) {
 }
 
-angular.module('Computers', ['ComputersServices']).config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/computer', {templateUrl:'partial/list.html', controller:ListComputerCtrl});
+var app = angular.module('Computers', ['ComputersServices']);
+
+app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/computers', {templateUrl:'partial/list.html', controller:ListComputerCtrl});
     $routeProvider.when('/computer/new', {templateUrl:'partial/editOrNew.html', controller:NewComputerCtrl});
     $routeProvider.when('/computer/:id', {templateUrl:'partial/editOrNew.html', controller:EditComputerCtrl});
-    $routeProvider.otherwise({redirectTo: '/computer'});
+    $routeProvider.otherwise({redirectTo: '/computers'});
 }]);
+
+app.directive('date', function (dateFilter) {
+    return {
+        require:'ngModel',
+        link:function (scope, elm, attrs, ctrl) {
+            var dateFormat = 'yyyy-MM-dd';
+            var dateRegexp = /^[1-2][0-9][0-9][0-9]\-[01][0-9]\-[0-3][0-9]$/;
+
+            ctrl.$parsers.unshift(function (viewValue) {
+                if (dateRegexp.test(viewValue)) {
+                    var parsedDateMilissec = Date.parse(viewValue);
+                    if (parsedDateMilissec > 0) {
+                        ctrl.$setValidity('date', true);
+                        return parsedDateMilissec;
+                    }
+                }
+
+                // in all other cases it is invalid, return undefined (no model update)
+                ctrl.$setValidity('date', false);
+                return undefined;
+            });
+
+            ctrl.$formatters.unshift(function (modelValue) {
+                return dateFilter(modelValue, dateFormat);
+            });
+        }
+    };
+});
 
 var services = angular.module('ComputersServices', ['ngResource']);
 
